0c26084eac72eaf5978cf821c6720a60caee249d
Laura Gautier <lc.gautier@icloud.com>
2025-07-29 10:59:57 +0200

    Exercice Partie 2 - Tableau avec to do list

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,5 +8,22 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         String name = scanner.nextLine();
         System.out.println("Hello, " + name);
+
+        String[] tasks = {
+                "Préparer la valise pour les vacances",
+                "Racheter des croquettes pour Gribouille",
+                "Faire le plein de la voiture",
+                "Faire quelques courses pour la semaine",
+                "Jouer à WoW",
+                "Terminer le dossier du projet fil rouge"
+        };
+
+        System.out.println("\nVoilà ta petite to do list de la semaine " + name + " !");
+
+        for (String task : tasks) {
+            System.out.println(task);
+        }
+
+        scanner.close();
     }
 }
